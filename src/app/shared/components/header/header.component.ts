33df17a319ed6b165f6a3ef83a94a306c9f8824c
Rafael Felipe <rafa_el_felipe@hotmail.com>
2024-04-07 13:41:52 -0300

    Adicionado dois icones

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faUser, faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
@@ -21,4 +21,6 @@ import { MatMenuModule } from '@angular/material/menu';
 })
 export class HeaderComponent {
   faUser = faUser;
+  faMoon = faMoon;
+  faSun = faSun;
 }
