2f2eac8f18dad89a4c6979295afcd89d53eafbfa
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2024-06-14 20:48:00 -0300

    ajustado style do botão de login

--- src/app/shared/components/login-button/login-button.component.ts
+++ src/app/shared/components/login-button/login-button.component.ts
@@ -2,16 +2,43 @@ import {Component, OnInit} from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import {AsyncPipe} from "@angular/common";
 import {Auth0ManagementService} from "../../services/Auth0ManagementService";
+import {faUser} from "@fortawesome/free-regular-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-login-button',
   template: `
-    <div>
-      <button (click)="login()">Log in</button>
-    </div>
+    <button
+      (click)="login()"
+      class="btn-session"
+      mat-button
+      aria-label="Profile"
+    >
+      <div class="flex-center">
+        <fa-icon class="session-icon" [icon]="faUser"></fa-icon>
+        <p>{{ "Iniciar Sessão" }}</p>
+      </div>
+    </button>
   `,
+  styles: `.btn-session {
+    height: 45px !important;
+    div {
+      gap: 10px;
+
+      fa-icon {
+        font-size: 20px;
+        color: var(--mdc-icon-button-icon-color)
+      }
+      p {
+        margin: 0;
+        font-size: 14px;
+        font-weight: 500;
+      }
+    }
+  }`,
   imports: [
-    AsyncPipe
+    AsyncPipe,
+    FaIconComponent
   ],
   standalone: true
 })
@@ -24,4 +51,6 @@ export class LoginButtonComponent{
   login() {
     this.auth.loginWithRedirect();
   }
+
+  protected readonly faUser = faUser;
 }
